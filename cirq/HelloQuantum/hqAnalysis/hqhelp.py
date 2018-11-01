8e4ebac6368565092d4d1e9bbde589e5cccb3d7e
jlmayfield <jlmayfield.4@gmail.com>
2018-10-31 21:14:48 -0500

    Updates to Analysis/Viz library for HQ Game
    A bigger step towards Issues #11.  This adds a couple of key things:
    a set of functions for computing state vectors and probability
    vectors for all the HelloQuantum measurement combos, a function
    hqGrid which plots the probability distributions of all four
    measurement combinations a la the center grid in HelloQuantum, and
    measurement specific functions for plot outcome probabilities for
    one or more steps.
    
    The big add here is hqGrid. This graphic lets you get a more
    probabilistic approach to the game's visualization. What's needed
    still are functions for tracing out individual bits and capturing
    the game's visualization at the bit level.

--- cirq/HelloQuantum/hqAnalysis/hqhelp.py
+++ cirq/HelloQuantum/hqAnalysis/hqhelp.py
@@ -9,6 +9,60 @@ Some useful functions for analyzing Hello Quantum Puzzles
 import numpy as np;
 import matplotlib.pyplot as plt
 
+#%%
+
+def toBell( st ):
+    H = np.array([[1,1],[1,-1]])
+    T = (0.5)*np.kron(H,H)    
+    
+    return T.dot(st)
+
+def toSB( st ):
+    H = (1/np.sqrt(2))*np.array([[1,1],[1,-1]])
+    T = np.kron(np.eye(2),H)    
+
+    return T.dot(st)
+
+def toBS( st ):
+    H = (1/np.sqrt(2))*np.array([[1,1],[1,-1]])
+    T = np.kron(H,np.eye(2))    
+
+    return T.dot(st)
+    
+
+#%%
+
+def pStd( st ):
+    """
+    Compute the measurement probabilities for a std/std measurement.
+    This gets at the southern circle of the center 4.
+    """    
+    return abs(st)**2    
+
+def pBell( st ):
+    """
+    Compute the measurement probabilities for a bell/bell measurement.
+    This gets at the northern circle of the center 4.
+    """    
+        
+    return abs(toBell(st))**2    
+
+def pSB( st ):
+    """
+    Compute the measurement probabilities for a std/bell measurement.
+    This gets at the eastern circle of the center 4.
+    """    
+    
+    return abs(toSB(st))**2    
+
+def pBS( st ):
+    """
+    Compute the measurement probabilities for a bell/std measurement.
+    This gets at the northern circle of the center 4.
+    """    
+        
+    return abs(toBS(st))**2        
+
 
 #%%
 
@@ -16,11 +70,10 @@ def pBarsStd( st ) :
     """
     Display the probability of measuring each of the four standard basis
     outcomes. 
-    """   
-    
+    """       
     
     fig, ax = plt.subplots()
-    ax.bar(np.arange(4), abs(st)**2 , 0.25, color='b')
+    ax.bar(np.arange(4), pStd(st) , 0.25, color='b')
     ax.set_xlabel('Standard Basis')
     ax.set_ylabel('Probability')
     ax.set_yticks([0,.25,.5,.75,1])    
@@ -36,11 +89,9 @@ def pBarsBell( st ) :
     Display the probability of measuring each of the four Bell basis
     outcomes. 
     """
-    H = np.array([[1,1],[1,-1]])
-    b_st=np.flip((1/2)*( np.kron(H,H).dot(np.flip(st)) ))
     
     fig, ax = plt.subplots()
-    ax.bar(np.arange(4), abs(b_st)**2 , 0.25, color='b')
+    ax.bar(np.arange(4), pBell(st) , 0.25, color='b')
     ax.set_xlabel('Bell Basis')
     ax.set_ylabel('Probability')
     ax.set_yticks([0,.25,.5,.75,1])
@@ -50,6 +101,42 @@ def pBarsBell( st ) :
     ax.set_ylim((0,1.1))
         
     plt.show()
+    
+def pBarsSB( st ) :
+    """
+    Display the probability of measuring each of the four std/bell 
+    hybrid basis
+    """
+        
+    fig, ax = plt.subplots()
+    ax.bar(np.arange(4), pSB(st) , 0.25, color='b')
+    ax.set_xlabel('Basis')
+    ax.set_ylabel('Probability')
+    ax.set_yticks([0,.25,.5,.75,1])
+    ax.set_title('Probability of Measuring the Std/Bell Basis')
+    ax.set_xticks(np.arange(4))
+    ax.set_xticklabels(('0+', '0-', '1+', '1-'))
+    ax.set_ylim((0,1.1))
+        
+    plt.show()    
+    
+def pBarsBS( st ) :
+    """
+    Display the probability of measuring each of the four std/bell 
+    hybrid basis
+    """
+        
+    fig, ax = plt.subplots()
+    ax.bar(np.arange(4), pBS(st) , 0.25, color='b')
+    ax.set_xlabel('Basis')
+    ax.set_ylabel('Probability')
+    ax.set_yticks([0,.25,.5,.75,1])
+    ax.set_title('Probability of Measuring the Bell/Std Basis')
+    ax.set_xticks(np.arange(4))
+    ax.set_xticklabels(('+0', '+1', '-0', '-1'))
+    ax.set_ylim((0,1.1))
+        
+    plt.show()    
 
 #%%
 
@@ -59,22 +146,22 @@ def pBarsStdAll( sim ) :
     the probability of each standard basis at each step.
     """
     
-    fig, ax = plt.subplots()
-    index = np.arange(4)
-    bar_width = .25
-    n_steps = 0
+    states = np.vstack( [s.state()for s in sim ]  )
     
-    for i, step in enumerate(sim):
-        n_steps = i
-        st = np.around(step.state(), 3)
-        ax.bar(index+(i*bar_width) , abs(st)**2, bar_width,
+    fig, ax = plt.subplots()    
+    bar_width = .25
+    index = np.arange(4)*(len(states)+1)*bar_width
+    
+    
+    for i,st in enumerate(states):        
+        ax.bar(index+(i*bar_width) , pStd(st), bar_width,
                label="step " + str(i+1))
         
     ax.set_xlabel('Standard Basis')        
     ax.set_ylabel('Probability')
     ax.set_yticks([0,.25,.5,.75,1])    
     ax.set_title('Probability of Measuring the Standard Basis')
-    ax.set_xticks(np.arange(4)+(n_steps*bar_width)/2)
+    ax.set_xticks(index+(len(states)*bar_width)/2)
     ax.set_xticklabels(('00', '01', '10', '11'))
     ax.set_ylim((0,1.1))
     ax.legend()
@@ -88,26 +175,22 @@ def pBarsBellAll( sim ) :
     the probability of each standard basis at each step.
     """
     
+    states = np.vstack( [s.state()for s in sim ]  )
+       
+    
     fig, ax = plt.subplots()
-    index = np.arange(4)
     bar_width = .25
-    n_steps = 0
+    index = np.arange(4)*(len(states)+1)*bar_width
     
-    H = np.array([[1,1],[1,-1]])
-    T = np.kron(H,H)    
-    
-    for i, step in enumerate(sim):
-        n_steps = i
-        st_std = np.around(step.state(), 3)
-        st_bell = np.flip((1/2)*T.dot(np.flip(st_std)))
-        ax.bar(index+(i*bar_width) , abs(st_bell)**2, bar_width,
+    for i,st in enumerate(states):        
+        ax.bar(index+(i*bar_width) , pBell(st) , bar_width,
                label="step " + str(i+1))
         
     ax.set_xlabel('Bell Basis')        
     ax.set_ylabel('Probability')
     ax.set_yticks([0,.25,.5,.75,1])    
     ax.set_title('Probability of Measuring the Bell Basis')
-    ax.set_xticks(np.arange(4)+(n_steps*bar_width)/2)
+    ax.set_xticks(index+(len(states)*bar_width)/2)
     ax.set_xticklabels(('++', '+-', '-+', '--'))
     ax.set_ylim((0,1.1))
     ax.legend()
@@ -115,5 +198,115 @@ def pBarsBellAll( sim ) :
     fig.tight_layout()
     plt.show()
 
+def pBarsSBAll( sim ) :
+    """
+    Take a moment step simulation generator and produce a graph showing
+    the probability of each ... std/bell hybrid basis
+    """
+    
+    states = np.vstack( [s.state()for s in sim ]  )    
+    
+    fig, ax = plt.subplots()
+    bar_width = .25
+    index = np.arange(4)*(len(states)+1)*bar_width
+    
+    for i,st in enumerate(states):        
+        ax.bar(index+(i*bar_width) , pSB(st) , bar_width,
+               label="step " + str(i+1))
         
+    ax.set_xlabel('Basis')        
+    ax.set_ylabel('Probability')
+    ax.set_yticks([0,.25,.5,.75,1])    
+    ax.set_title('Probability of Measuring in a Std/Bell Hybrid Basis')
+    ax.set_xticks(index+(len(states)*bar_width)/2)
+    ax.set_xticklabels(('0+', '0-', '1+', '1-'))
+    ax.set_ylim((0,1.1))
+    ax.legend()
+    
+    fig.tight_layout()
+    plt.show()
 
+def pBarsBSAll( sim ) :
+    """
+    Take a moment step simulation generator and produce a graph showing
+    the probability of each ... bell/std hybrid basis
+    """
+    
+    states = np.vstack( [s.state()for s in sim ]  )
+    
+    fig, ax = plt.subplots()
+    bar_width = .25
+    index = np.arange(4)*(len(states)+1)*bar_width
+    
+    for i,st in enumerate(states):        
+        ax.bar(index+(i*bar_width) , pBS(st) , bar_width,
+               label="step " + str(i+1))
+        
+    ax.set_xlabel('Basis')        
+    ax.set_ylabel('Probability')
+    ax.set_yticks([0,.25,.5,.75,1])    
+    ax.set_title('Probability of Measuring in a Bell/Std Hybrid Basis')
+    ax.set_xticks(index+(len(states)*bar_width)/2)
+    ax.set_xticklabels(('+0', '+1', '-0', '-1'))
+    ax.set_ylim((0,1.1))
+    ax.legend()
+    
+    fig.tight_layout()
+    plt.show()        
+
+#%%
+    
+
+def hqGrid( st , tofile=False,name=""):
+    """
+    Plot probability distributions for the two qubit measurment outcomes
+    Grid for Hello Quantum
+    """
+    
+    fig,ax = plt.subplots(2,2)
+    
+     #North Bell
+    ax[0][0].bar(np.arange(4), pBell(st) , 0.25, color='b')
+    ax[0][0].set_xlabel('Bell Basis')
+    ax[0][0].set_ylabel('Probability')
+    ax[0][0].set_yticks([0,.25,.5,.75,1])
+    ax[0][0].set_title('Bell Measurement')
+    ax[0][0].set_xticks(np.arange(4))
+    ax[0][0].set_xticklabels(('++', '+-', '-+', '--'))
+    ax[0][0].set_ylim((0,1.1))
+    
+    #West BS
+    ax[1][0].bar(np.arange(4), pBS(st) , 0.25, color='b')
+    ax[1][0].set_xlabel('Basis')
+    ax[1][0].set_ylabel('Probability')
+    ax[1][0].set_yticks([0,.25,.5,.75,1])
+    ax[1][0].set_title('Bell/Std Measurement')
+    ax[1][0].set_xticks(np.arange(4))
+    ax[1][0].set_xticklabels(('+0', '+1', '-0', '-1'))
+    ax[1][0].set_ylim((0,1.1))
+    
+    #East SB
+    ax[0][1].bar(np.arange(4), pSB(st) , 0.25, color='b')
+    ax[0][1].set_xlabel('Basis')
+    ax[0][1].set_ylabel('Probability')
+    ax[0][1].set_yticks([0,.25,.5,.75,1])
+    ax[0][1].set_title('Std/Bell Measurement')
+    ax[0][1].set_xticks(np.arange(4))
+    ax[0][1].set_xticklabels(('0+', '0-', '1+', '1-'))
+    ax[0][1].set_ylim((0,1.1))
+    
+    #South Std
+    ax[1][1].bar(np.arange(4), pStd(st) , 0.25, color='b')
+    ax[1][1].set_xlabel('Standard Basis')
+    ax[1][1].set_ylabel('Probability')
+    ax[1][1].set_yticks([0,.25,.5,.75,1])    
+    ax[1][1].set_title('Standard Measurement')
+    ax[1][1].set_xticks(np.arange(4))
+    ax[1][1].set_xticklabels(('00', '01', '10', '11'))
+    ax[1][1].set_ylim((0,1.1))
+    
+    
+    fig.tight_layout()
+    if tofile :
+        plt.savefig(name)
+    plt.show()
